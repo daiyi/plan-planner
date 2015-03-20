1e3dbf47f1ff57899368dceb20873c3f266d1835
daiyi <clsun91@gmail.com>
2015-03-20 16:29:53 -0700

    cleaned index page, cleaned 'show' page

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,6 @@
 class Plan < ActiveRecord::Base
   has_attached_file :image, styles: { tile: "360x360" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates :image, presence: true
   validates :title, presence: true
 end
